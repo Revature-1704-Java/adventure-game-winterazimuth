0bdef193095b0abc192082ba5cee5a1677502a6e
James Widener <jameskwidener@gmail.com>
2017-12-10 21:57:32 -0500

    Game now makes GameBuilder, Parser, and Player objects.

--- src/main/java/com/revature/Game.java
+++ src/main/java/com/revature/Game.java
@@ -1,13 +1,12 @@
 package com.revature;
 
-/**
- * Hello world!
- *
- */
 public class Game 
 {
     public static void main( String[] args )
     {
+        Player you = new Player();
+        GameBuilder coffeeQuest = new GameBuilder();
+        Parser io = new Parser();
         System.out.println( "Hello World!" );
     }
 }
